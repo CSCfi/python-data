0982efaab199765336edd4562734468a81b4f0e5
Jyry Suvilehto <jyry.suvilehto@csc.fi>
2016-11-21 23:10:30 +0200

    more data

--- bootstrap/hamk_2.bash
+++ bootstrap/hamk_2.bash
@@ -8,6 +8,9 @@ cd data
 wget https://vincentarelbundock.github.io/Rdatasets/csv/ISLR/Wage.csv
 wget https://vincentarelbundock.github.io/Rdatasets/csv/car/Davis.csv
 wget https://raw.githubusercontent.com/CSC-IT-Center-for-Science/data-stat-course/master/datasets/weather-kumpula.csv
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/00320/student.zip
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv
 cd hml
 wget http://www.hameenlinna.fi/pages/563719/Ostolaskut/2014/HML%20ostolaskut%202014%20julkaisu%20csv.zip
 unzip *.zip
